8744a41a8c38fa80b2c324f6dd48c8b8b4f44a5c
MichelleGunawan <michellegunawan9999@gmail.com>
2024-05-13 12:45:28 -0700

    My portfolio

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { Helmet } from "react-helmet";
+import { Box, Tabs, Tab } from "@mui/material";
 
 import NavBar from "../components/common/navBar";
 import Footer from "../components/common/footer";
@@ -8,6 +9,7 @@ import AllProjects from "../components/projects/allProjects";
 
 import INFO from "../data/user";
 import SEO from "../data/seo";
+import projects from "../data/projects";
 
 import "./styles/projects.css";
 
@@ -17,6 +19,28 @@ const Projects = () => {
 	}, []);
 
 	const currentSEO = SEO.find((item) => item.page === "projects");
+
+	const [value, setValue] = React.useState(0);
+
+	const handleChange = (event, newValue) => {
+		setValue(newValue);
+	};
+
+	function CustomTabPanel(props) {
+		const { children, value, index, ...other } = props;
+
+		return (
+			<div
+				role="tabpanel"
+				hidden={value !== index}
+				id={`simple-tabpanel-${index}`}
+				aria-labelledby={`simple-tab-${index}`}
+				{...other}
+			>
+				{value === index && <Box sx={{ p: 0 }}>{children}</Box>}
+			</div>
+		);
+	}
 
 	return (
 		<React.Fragment>
@@ -44,20 +68,81 @@ const Projects = () => {
 						</div>
 
 						<div className="subtitle projects-subtitle">
-							I've worked on a variety of projects over the years
-							and I'm proud of the progress I've made. Many of
-							these projects are open-source and available for
-							others to explore and contribute to. If you're
-							interested in any of the projects I've worked on,
-							please feel free to check out the code and suggest
-							any improvements or enhancements you might have in
-							mind. Collaborating with others is a great way to
-							learn and grow, and I'm always open to new ideas and
-							feedback.
+							I've worked on a variety of projects over the years,
+							and I'm proud of the progress I've made. Many of the
+							projects I've worked on stem from a desire to
+							address real-life needs and streamline challenges
+							I've encountered myself or observed others grappling
+							with in their everyday lives. If you're curious
+							about any of the projects I've worked on or have any
+							suggestions on improvements or enhancements, feel
+							free to reach out. Collaborating with others is a
+							great way to learn and grow, and I'm always open to
+							new ideas and feedback.
 						</div>
 
 						<div className="projects-list">
-							<AllProjects />
+							<Tabs
+								value={value}
+								onChange={handleChange}
+								indicatorColor="transparent"
+								centered
+							>
+								<Tab
+									label="All"
+									className="tab"
+									style={{
+										color:
+											value === 0 ? "#14b8a6" : "#65656d",
+										fontSize: "16px",
+										lineHeight: "28px",
+										borderRadius: "15px",
+										fontWeight: "400",
+									}}
+								/>
+								<Tab
+									label="Web"
+									className="tab"
+									style={{
+										color:
+											value === 1 ? "#14b8a6" : "#65656d",
+										fontSize: "16px",
+										lineHeight: "28px",
+										borderRadius: "15px",
+										fontWeight: "400",
+									}}
+								/>
+								<Tab
+									label="Mobile"
+									className="tab"
+									style={{
+										color:
+											value === 2 ? "#14b8a6" : "#65656d",
+										fontSize: "16px",
+										lineHeight: "28px",
+										borderRadius: "15px",
+										fontWeight: "400",
+									}}
+								/>
+							</Tabs>
+
+							<CustomTabPanel value={value} index={0}>
+								<AllProjects projects={projects} />
+							</CustomTabPanel>
+							<CustomTabPanel value={value} index={1}>
+								<AllProjects
+									projects={projects.filter(
+										(project) => project.type === "web"
+									)}
+								/>
+							</CustomTabPanel>
+							<CustomTabPanel value={value} index={2}>
+								<AllProjects
+									projects={projects.filter(
+										(project) => project.type === "mobile"
+									)}
+								/>
+							</CustomTabPanel>
 						</div>
 					</div>
 					<div className="page-footer">
--- src/pages/readArticle.jsx
+++ src/pages/readArticle.jsx
@@ -9,31 +9,33 @@ import Logo from "../components/common/logo";
 
 import INFO from "../data/user";
 import myArticles from "../data/articles";
+import { faChevronCircleLeft } from "@fortawesome/free-solid-svg-icons";
 
 import "./styles/readArticle.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 let ArticleStyle = styled.div``;
 
 const ReadArticle = () => {
-	const navigate = useNavigate();
 	let { slug } = useParams();
 
-	const article = myArticles[slug - 1];
+	// Find the project object based on its route property
+	const article = myArticles.find((article) => article.route === slug);
 
 	useEffect(() => {
 		window.scrollTo(0, 0);
 	}, [article]);
 
 	ArticleStyle = styled.div`
-		${article().style}
+		${article.style}
 	`;
 
 	return (
 		<React.Fragment>
 			<Helmet>
-				<title>{`${article().title} | ${INFO.main.title}`}</title>
-				<meta name="description" content={article().description} />
-				<meta name="keywords" content={article().keywords.join(", ")} />
+				<title>{`${article.title} | ${INFO.main.title}`}</title>
+				<meta name="description" content={article.description} />
+				<meta name="keywords" content={article.keywords.join(", ")} />
 			</Helmet>
 
 			<div className="page-content">
@@ -47,28 +49,38 @@ const ReadArticle = () => {
 					</div>
 
 					<div className="read-article-container">
-						<div className="read-article-back">
+						{/* <div className="read-article-back">
 							<img
 								src="../back-button.png"
 								alt="back"
 								className="read-article-back-button"
 								onClick={() => navigate(-1)}
 							/>
-						</div>
+						</div> */}
 
 						<div className="read-article-wrapper">
-							<div className="read-article-date-container">
-								<div className="read-article-date">
-									{article().date}
+							<div className="read-article-admin">
+								<FontAwesomeIcon
+									icon={faChevronCircleLeft}
+									className="read-article-back-button"
+									onClick={() =>
+										(window.location.href =
+											"https://michellegunawan.github.io/portfolio/#/articles")
+									}
+								/>
+								<div className="read-article-date-container">
+									<div className="read-article-date">
+										{article.date}
+									</div>
 								</div>
 							</div>
 
 							<div className="title read-article-title">
-								{article().title}
+								{article.title}
 							</div>
 
 							<div className="read-article-body">
-								<ArticleStyle>{article().body}</ArticleStyle>
+								<ArticleStyle>{article.body}</ArticleStyle>
 							</div>
 						</div>
 					</div>
